3c9ed2694e8398fdb20c6dd9ab5c5f8f41fa8683
Ian Petersen <ispeters@gmail.com>
2026-05-16 15:23:34 -0700

    Reorganize with_frame_allocator
    This makes each free list own its own allocations; I mostly like it
    better, but it does mean that supporting a fallback of anything other
    than `::operator new` probably requires redundant storage of the
    fallback mechanism (such as a `memory_resource* __upstream_`) so that
    `allocate` and the destructor can properly retrieve and return new
    chunks.

--- include/exec/with_frame_allocator.hpp
+++ include/exec/with_frame_allocator.hpp
@@ -27,130 +27,143 @@ namespace experimental::execution
   {
     using namespace STDEXEC;
 
-    struct __unsynchronized_stack
+    struct __unsynchronized_recycling_resource : std::pmr::memory_resource
     {
-      constexpr bool empty() const noexcept
+      constexpr __unsynchronized_recycling_resource() noexcept = default;
+
+      STDEXEC_IMMOVABLE(__unsynchronized_recycling_resource);
+
+      constexpr ~__unsynchronized_recycling_resource()
       {
-        return __stack_.empty();
+        for (auto* __chnk = __list_.front(); __chnk != nullptr;)
+        {
+          auto [__next, __size, __align] = *__chnk;
+          ::operator delete(static_cast<void*>(__chnk), __size, __align);
+          __chnk = __next;
+        }
       }
 
-      constexpr void push(void* __p) noexcept
+      constexpr void* allocate(std::size_t __bytes, std::size_t __alignment)
       {
-        __stack_.push_front(new (__p) __node());
+        STDEXEC_ASSERT(__bytes >= sizeof(__chunk));
+
+        auto __align = static_cast<std::align_val_t>(__alignment);
+
+        if (__list_.empty())
+        {
+          return ::operator new(__bytes, __align);
+        }
+        else
+        {
+          __chunk* __ret = __list_.pop_front();
+
+          STDEXEC_ASSERT(__ret->__size_ == __bytes);
+          STDEXEC_ASSERT(__ret->__align_ == __align);
+
+          return __ret;
+        }
       }
 
-      constexpr void* pop() noexcept
+      constexpr void deallocate(void* __p, std::size_t __bytes, std::size_t __align) noexcept
       {
-        return __stack_.pop_front();
-      }
+        __chunk* __chnk = new (__p) __chunk(__bytes, std::align_val_t(__align));
 
-      constexpr auto begin() noexcept
-      {
-        return __stack_.begin();
-      }
-
-      constexpr auto end() noexcept
-      {
-        return __stack_.end();
+        __list_.push_front(__chnk);
       }
 
      private:
-      struct __node
+      struct __chunk
       {
-        __node* __next_{};
+        __chunk* __next_{nullptr};
+
+        std::size_t      __size_;
+        std::align_val_t __align_;
+
+        constexpr explicit __chunk(std::size_t __size, std::align_val_t __align) noexcept
+          : __size_(__size)
+          , __align_(__align)
+        {}
       };
 
-      __intrusive_slist<&__node::__next_> __stack_;
+      __intrusive_slist<&__chunk::__next_> __list_;
+
+      constexpr void* do_allocate(std::size_t __bytes, std::size_t __align) final
+      {
+        return allocate(__bytes, __align);
+      }
+
+      constexpr void do_deallocate(void* __p, std::size_t __bytes, std::size_t __align) final
+      {
+        deallocate(__p, __bytes, __align);
+      }
+
+      constexpr bool do_is_equal(std::pmr::memory_resource const & __other) const noexcept final
+      {
+        return this == &__other;
+      }
     };
 
-    template <class _Stack>
-    concept __free_stack =        //
-      std::ranges::range<_Stack>  //
-      && requires(_Stack& __stack, _Stack const & __cstack) {
-           { __cstack.empty() } -> __same_as<bool>;
-           { __stack.pop() } -> __same_as<void*>;
-           { __stack.push(static_cast<void*>(nullptr)) };
-         };
+    template <class _Resource>
+    concept __memory_resource = requires(_Resource& __resource) {
+      { __resource.allocate(std::size_t{}, std::size_t{}) } -> __same_as<void*>;
+      { __resource.deallocate((void*) nullptr, std::size_t{}, std::size_t{}) } -> __same_as<void>;
+    };
 
-    static_assert(__free_stack<__unsynchronized_stack>);
-
-    template <__free_stack _FreeStack = __unsynchronized_stack>
-    struct __free_stack_recycler : std::pmr::memory_resource
+    template <__memory_resource _Resource = __unsynchronized_recycling_resource>
+    struct __chunked_resource : std::pmr::memory_resource
     {
-      constexpr __free_stack_recycler() noexcept = default;
+      constexpr __chunked_resource() noexcept = default;
 
-      STDEXEC_IMMOVABLE(__free_stack_recycler);
+      STDEXEC_IMMOVABLE(__chunked_resource);
 
-      constexpr ~__free_stack_recycler()
+      constexpr ~__chunked_resource() = default;
+
+      constexpr void* allocate(std::size_t __bytes, std::size_t __align)
       {
-        std::size_t __size = __smallest_allocation;
-        for (auto& __stack: __free_stacks_)
+        // delegate to the appropriate chunk if __bytes is small enough
+        if (auto __chunk_index = __chunk_index_of(__bytes); __chunk_index < __chunk_count)
         {
-          while (!__stack.empty())
-          {
-            void* __allocation = __stack.pop();
-            ::operator delete(__allocation, __size);
-          }
-
-          __size <<= 1;
+          return __chunks_[__chunk_index].allocate(__bytes, __align);
+        }
+        else
+        {
+          // __bytes is bigger than the largest chunk so just punt to ::new
+          return ::operator new(__bytes, static_cast<std::align_val_t>(__align));
         }
       }
 
-      constexpr void* allocate(std::size_t __n, std::size_t)
+      constexpr void deallocate(void* __p, std::size_t __bytes, std::size_t __align) noexcept
       {
-        // check if there's a free stack that might contain an allocation we can reuse
-        if (auto __stack_index = __free_stack_index_of(__n);
-            __stack_index < __allocation_size_count)
+        if (auto __chunk_index = __chunk_index_of(__bytes); __chunk_index < __chunk_count)
         {
-          // check if that stack has an allocation we can reuse
-          if (auto& __free_stack = __free_stacks_[__stack_index]; !__free_stack.empty())
-          {
-            return __free_stack.pop();
-          }
-          else
-          {
-            // allocate more bytes than asked for so that we can put this allocation back
-            // into a free stack when it's deallocated
-            return ::operator new(__allocation_size_of(__n));
-          }
-        }
-
-        // __n is bigger than the largest cached allocation so just punt to ::new
-        return ::operator new(__n);
-      }
-
-      constexpr void deallocate(void* __p, std::size_t __n, std::size_t) noexcept
-      {
-        if (auto __stack_index = __free_stack_index_of(__n);
-            __stack_index < __allocation_size_count)
-        {
-          // cache the allocation addressed by __p for later use
           STDEXEC_TRY
           {
-            __free_stacks_[__stack_index].push(__p);
-            return;
+            // delegate deallocation to the appropriate chunk
+            __chunks_[__chunk_index].deallocate(__p, __bytes, __align);
           }
           STDEXEC_CATCH(...)
           {
-            // oops; fall through to the delete, below
-            goto do_delete;
+            // deallocate is required to "throw nothing" but is not marked noexcept...
+            std::unreachable();
           }
         }
-
-do_delete:
-        // __p points at an allocation that's too big for us to have cached
-        ::operator delete(__p, __n);
+        else
+        {
+          // __p points at an allocation that's too big for us to have cached
+          ::operator delete(__p, __bytes, static_cast<std::align_val_t>(__align));
+        }
       }
 
      private:
-      constexpr void* do_allocate(std::size_t __n, std::size_t __a) final
+      constexpr void* do_allocate(std::size_t __bytes, std::size_t __align) final
       {
-        return allocate(__n, __a);
+        return allocate(__bytes, __align);
       }
 
-      constexpr void do_deallocate(void* __p, std::size_t __n, std::size_t __a) noexcept final
+      constexpr void
+      do_deallocate(void* __p, std::size_t __bytes, std::size_t __align) noexcept final
       {
-        return deallocate(__p, __n, __a);
+        return deallocate(__p, __bytes, __align);
       }
 
       constexpr bool do_is_equal(std::pmr::memory_resource const & __other) const noexcept final
@@ -159,33 +172,26 @@ do_delete:
       }
 
       // the number of bytes in the smallest allocation
-      static constexpr std::size_t __smallest_allocation = 64;
+      static constexpr std::size_t __smallest_chunk = 64;
       // the number of allocation sizes before falling back to ::new
-      static constexpr std::size_t __allocation_size_count = 10;
+      static constexpr std::size_t __chunk_count = 10;
 
       static constexpr std::size_t __allocation_size_of(std::size_t __bytes) noexcept
       {
         // round __bytes up to the nearest power of two that is at least
-        // __smallest_allocation
-        return std::bit_ceil(__bytes | __smallest_allocation);
+        // __smallest_chunk
+        return std::bit_ceil(__bytes | __smallest_chunk);
       }
 
-      static constexpr std::size_t __free_stack_index_of(std::size_t __bytes) noexcept
+      static constexpr std::size_t __chunk_index_of(std::size_t __bytes) noexcept
       {
-        // compute the index into __free_stacks_ containing the cached allocations for
+        // compute the index into __chunks_ containing the cached allocations for
         // allocations of size __bytes; returns an index larger than the largest valid
         // index for values of __bytes whose allocations will not be cached
-        return std::countr_zero(__allocation_size_of(__bytes))
-             - std::countr_zero(__smallest_allocation);
+        return std::countr_zero(__allocation_size_of(__bytes)) - std::countr_zero(__smallest_chunk);
       }
 
-      std::array<_FreeStack, __allocation_size_count> __free_stacks_{};
-    };
-
-    template <class _Resource>
-    concept __memory_resource = requires(_Resource& __resource) {
-      { __resource.allocate(std::size_t{}, std::size_t{}) } -> __same_as<void*>;
-      { __resource.deallocate((void*) nullptr, std::size_t{}, std::size_t{}) } -> __same_as<void>;
+      std::array<_Resource, __chunk_count> __chunks_{};
     };
 
     template <__memory_resource _Resource>
@@ -220,7 +226,7 @@ do_delete:
       }
     };
 
-    template <class _Ty, __memory_resource _Resource = __free_stack_recycler<>>
+    template <class _Ty, __memory_resource _Resource = __chunked_resource<>>
     struct __frame_allocator : __resource<_Resource>
     {
       using value_type = _Ty;
@@ -242,8 +248,8 @@ do_delete:
     template <class _Receiver>
     struct __opstate
     {
-      _Receiver                       __rcvr_;
-      mutable __free_stack_recycler<> __resource_;
+      _Receiver                    __rcvr_;
+      mutable __chunked_resource<> __resource_;
     };
 
     struct with_frame_allocator_t : sender_adaptor_closure<with_frame_allocator_t>
