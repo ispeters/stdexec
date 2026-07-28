253916b3d569aa579bb63067fc6dea2ad2ee4bf0
Ian Petersen <ispeters@gmail.com>
2026-07-27 23:30:38 -0700

    Make more tests pass
    This diff defaults modules builds to demangle sender names, working
    around what seems to be a Clang bug where it gives up generating new
    mangled names for lambdas, and exports a couple of
    descriptor/demangling-related symbols so that sequence-senders can use
    them.

--- include/stdexec/__detail/__config.hpp
+++ include/stdexec/__detail/__config.hpp
@@ -925,7 +925,7 @@ namespace STDEXEC
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-#if !defined(STDEXEC_DEMANGLE_SENDER_NAMES) && STDEXEC_MSVC()
+#if !defined(STDEXEC_DEMANGLE_SENDER_NAMES) && (STDEXEC_MSVC() || STDEXEC_USE_MODULES())
 #  define STDEXEC_DEMANGLE_SENDER_NAMES
 #endif
 
